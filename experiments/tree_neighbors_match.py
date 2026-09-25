7fb702c51d263dcf6de7f0fe11b83de0455fec64
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2026-09-25 14:22:22 -0500

    Update torch.load and torch.save calls in tree_neighbors_match

--- experiments/tree_neighbors_match.py
+++ experiments/tree_neighbors_match.py
@@ -15,7 +15,7 @@ class TreeNeighborsMatch(InMemoryDataset):
         self.depth = depth
         self.num_graphs = num_graphs
         super().__init__(root, transform, pre_transform)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+        self.data, self.slices = torch.load(self.processed_paths[0], weights_only=False)
 
     @property
     def processed_file_names(self):
